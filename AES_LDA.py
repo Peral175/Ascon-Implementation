77ec0a036acaffaf8b6b7740f83c9c6c4286db06
Peral175 <perrardalex@yahoo.com>
2024-06-19 17:38:52 +0200

    snapshot before refactoring

--- AES_LDA.py
+++ AES_LDA.py
@@ -1,12 +1,17 @@
+#!/usr/bin/env sage -python
 import argparse
 import pathlib
 import numpy as np
 import multiprocessing
 import datetime
 import os.path
-from sage.all import *
+
+from bitarray import frozenbitarray
+from sage.all import matrix, vector, GF
+from line_profiler import profile
 
 
+@profile
 def attack(T, trace_dir, w_size, step):
     numOfBytes = os.path.getsize(trace_dir / "0000.bin")
     numOfNodes = numOfBytes * 8
@@ -18,32 +23,85 @@ def attack(T, trace_dir, w_size, step):
     # print("window step = ", step)
 
     AES_SBox = [
-            0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
-            0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
-            0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15,
-            0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75,
-            0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84,
-            0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF,
-            0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8,
-            0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2,
-            0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73,
-            0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB,
-            0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79,
-            0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08,
-            0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A,
-            0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E,
-            0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
-            0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16
-        ]
+        0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
+        0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
+        0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15,
+        0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75,
+        0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84,
+        0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF,
+        0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8,
+        0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2,
+        0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73,
+        0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB,
+        0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79,
+        0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08,
+        0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A,
+        0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E,
+        0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
+        0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16
+    ]
 
     def selection_function(ptb, kbg):
         return AES_SBox[ptb ^ kbg] & 1
+
+    # from bitarray import frozenbitarray, bitarray
+    # from itertools import product
+    # bit_array = bitarray
+    # bit_array_freeze = frozenbitarray
+    # ones = bit_array([1] * T)
+    # vec_ones = ones = bit_array_freeze(ones)
+    #
+    # def scalar_bin(a, b):
+    #     v = a & b
+    #     res = 0
+    #     while v:
+    #         res ^= v & 1
+    #         v >>= 1
+    #     return res
+    #
+    # scalar_map = [
+    #     [scalar_bin(x, lin) for x in range(256)]
+    #     for lin in range(256)
+    # ]
+    # targets = []
+    #
+    # PLAINTEXTS = []
+    # for i in range(T):
+    #     PLAINTEXTS.append(open(trace_dir / ("%04d.pt" % i), "rb").read())
+    #
+    # for si, lin, k in product((0,), (1,), range(0, 256)):
+    #     # iterates over dot product
+    #     # 0 1 0
+    #     # 0 1 1
+    #     # ...
+    #     target = bit_array(T)
+    #     scalar_lin = scalar_map[lin]
+    #     for itrace, p in enumerate(PLAINTEXTS):
+    #         if k is None:
+    #             x = p[si]
+    #         else:
+    #             x = p[si]
+    #             x = AES_SBox[x ^ k]
+    #         target[itrace] = scalar_lin[x]
+    #
+    #     target = bit_array_freeze(target)
+    #     targets.append((target, (si, lin, k, 0)))
+    #     targets.append((target ^ ones, (si, lin, k, 1)))
+    #
+    # print(targets)
+    # input(len(targets))
 
     PLAINTEXTS = []
     for traceNr in range(T):
         pt = trace_dir / ("%04d.pt" % traceNr)
         with open(pt, "rb") as f:
             PLAINTEXTS += [f.read(16)]
+
+    TRACES = []
+    for traceNumber in range(T):
+        ftrace = trace_dir / ("%04d.bin" % traceNumber)
+        with open(ftrace, "rb") as f:
+            TRACES += [f.read(numOfBytes)]
 
     dictionary = {}
     for plaintextByte in range(16):         # 16 bytes of key are attacked individually
@@ -53,74 +111,99 @@ def attack(T, trace_dir, w_size, step):
                 guessedVector ^= selection_function(PLAINTEXTS[traceNr][plaintextByte], keyByteGuess) << traceNr
             dictionary[guessedVector] = plaintextByte * 256 + keyByteGuess
 
-    TRACES = []
-    for traceNumber in range(T):
-        ftrace = trace_dir / ("%04d.bin" % traceNumber)
-        with open(ftrace, "rb") as f:
-            TRACES += [f.read(numOfBytes)]
-
     M = []
     for node in range(numOfNodes):
         nodeVector = 0
         for traceNumber in range(T):
             nodeVector ^= ((TRACES[traceNumber][node // 8] >> node % 8) & 0b1) << traceNumber
         M.append(nodeVector)
-    matr = np.zeros((T, numOfNodes), dtype=int, order='C')
+    matr = np.zeros((numOfNodes, T), dtype=int, order='C')
     for i in range(numOfNodes):
         c = bin(M[i])[2:].zfill(T)
         for j in range(T):
-            matr[j, i] = c[j]
+            matr[i, j] = c[j]
 
-    S = np.ndarray((16, T, 256), dtype=int, order='C')
+    S = np.ndarray((16, 256, T), dtype=int, order='C')
     l_dict = list(dictionary)
     for k in range(0, 4096, 256):
         c = l_dict[k:k+256]
         for m in range(256):
             d = bin(c[m])[2:].zfill(T)
             for n in range(T):
-                S[k//256, n, m] = d[n]
+                S[k//256, m, n] = d[n]
 
+    @profile
     def work(s, M_matrix, ID, numNodes, Solutions):
         for w in range(0, numNodes-w_size+1, step):
-            tmp = np.ascontiguousarray(M_matrix[:, w:w+w_size])
+            print("window:", w, "/", numNodes)
+            # tmp = np.ascontiguousarray(M_matrix[:, w:w+w_size])
+            tmp = M_matrix[w:w+w_size]
+            # print(tmp.shape)
             window = matrix(GF(2), tmp)
 
-            # todo: measure exactly how long partial window takes and think how to improve
+            TEST2 = window.right_kernel().matrix()          # only gives 500 why?
+            # TEST2 = window.left_kernel().matrix()     # 22 seconds isw2 vs 41 seconds before
+            TEST2 = [frozenbitarray(row) for row in TEST2]
 
-            # TEST1 = window.right_kernel().matrix()
-            TEST2 = window.left_kernel().matrix()     # 22 seconds isw2 vs 41 seconds before
+            # todo important
+            # visualize on white board
+            # precompute target + understand their code + measure performance with theirs + plot
 
-            # print("Test1: ", TEST1.dimensions())
-            # print("Test2: ", TEST2.dimensions())
+            # optimized implementation : check bit-by-bit, O(n^3 + nk)
+            for kg, target in enumerate(s):
+                # print(kg, len(target))
+                original = target
+                tmp = ''
+                for e in target:
+                    tmp += str(e)
+                target = frozenbitarray(tmp)
+                match = True
+                nm = 0
+                for row in TEST2:
+                    # if row * target:
+                    # print("Row: ", row, target, len(row), len(target), s.shape)
+                    if (row & target).count(1) & 1:
+                        match = False
+                        break
+                    nm += 1
+                if not match:
+                    continue
+                # sol = window.solve_right(vector(GF(2), original))
+                sol = window.solve_left(vector(GF(2), original))
+                # assert sol * mat == target
+                # print("Solution found:", sol)
+                Solutions[ID] = (w, kg)  # fastest way?
 
-            for kg in range(0, 256, 1):     # 2^8
-                K = vector(GF(2), s[:, kg])
-                vec = TEST2 * K
-                # # print("2!", vec, w, kg, type(vec[0]))
-                vec = set(vec)
-                if 1 not in vec:
-                    Solutions[ID] = (w, kg)  # fastest way?
-                    # return
+            # for kg in range(0, 256, 1):     # 2^8
+            #     # K = vector(GF(2), s[:, kg])
+            #     K = vector(GF(2), s[kg])
+            #     vec = TEST2 * K
+            #     # # print("2!", vec, w, kg, type(vec[0]))
+            #     vec = set(vec)
+            #     if 1 not in vec:
+            #         Solutions[ID] = (w, kg)  # fastest way?
+            #         # return
 
-                # try:
-                #     _ = window.solve_right(K)
-                #     Solutions[ID] = (w, kg)
-                #     # print("1!", _, w, kg)
-                #     # return
-                # except ValueError:
-                #     continue
+            # try:
+            #     _ = window.solve_right(K)
+            #     Solutions[ID] = (w, kg)
+            #     # print("1!", _, w, kg)
+            #     # return
+            # except ValueError:
+            #     continue
 
     SOLS = multiprocessing.Manager().dict()
-    procs = []
-    start = datetime.datetime.now()
-    for id in range(16):
-        proc = multiprocessing.Process(target=work, args=(S[id, :], matr, id, numOfNodes, SOLS,))
-        procs.append(proc)
-        proc.start()
-    for proc in procs:
-        proc.join()
-    end = datetime.datetime.now()
-    print("Time: ", end - start)
+    print(S[0,:].shape)
+    print(matr.shape)
+    work(S[0, :], matr, 0, numOfNodes, SOLS)
+
+    # procs = []
+    # for id in range(16):
+    #     proc = multiprocessing.Process(target=work, args=(S[id, :], matr, id, numOfNodes, SOLS,))
+    #     procs.append(proc)
+    #     proc.start()
+    # for proc in procs:
+    #     proc.join()
 
     if len(SOLS) == 16:
         recovered_key = ''
@@ -129,6 +212,8 @@ def attack(T, trace_dir, w_size, step):
         print("Recovered key: ", recovered_key)
         return True, recovered_key
     else:
+        print(SOLS)
+        print(chr(SOLS.get(0)[1]))
         return False, len(SOLS)
 
 
@@ -164,7 +249,25 @@ if __name__ == '__main__':
         help='sliding window size step'
     )
     args = parser.parse_args()
-    s1 = datetime.datetime.now()
+
+    start = datetime.datetime.now()
     attack(args.n_traces, args.trace_dir, args.window_size, args.step)
-    s2 = datetime.datetime.now()
-    print("Time of whole program: ", s2 - s1)
+    end = datetime.datetime.now()
+    print("Time: ", end - start)
+
+    """
+    Results for:    aes lda with 2 rounds clear
+    python3 AES_LDA.py traces/abcdefghABCDEFGH/aes2-clear/ -T 512 -W 500 -S 500
+    Time: 0:00:04.319435        VS      Time:  0:00:06.950022   [Better: Time:  0:00:05.640891]
+    We have around 20 % slower performance for clear
+    python3 AES_LDA.py traces/abcdefghABCDEFGH/aes2-isw2/ -T 512 -W 500 -S 250
+    Time: 0:00:22.076182        VS      Time:  0:00:35.938154
+    We have around 40 % slower performance for isw2
+    python3 AES_LDA.py traces/abcdefghABCDEFGH/aes2-isw2/ -T 300 -W 256 -S 128
+    Time: 0:00:16.654401        VS      Time:  0:00:22.093473   [Better: Time:  0:00:22.872240]
+    We have around 27% % slower performance for isw2
+
+    Detailed timing analysis:
+    kernprof -l AES_LDA.py traces/abcdefghABCDEFGH/aes2-clear/ -T 512 -W 500 -S 500
+    python3 -m line_profiler -rmt "AES_LDA.py.lprof"
+    """
