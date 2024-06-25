a4d97448d24003a05de0b79152fadf6ffdea84de
Peral175 <perrardalex@yahoo.com>
2024-06-25 14:40:04 +0200

    increased nr_traces for experiments

--- aes_script.sh
+++ aes_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-nr_traces=512
-window_size=256
+nr_traces=2098
 key=abcdefghABCDEFGH
+#key='0123456789:;<=>?'
 
 ## 1. run aes with key as cmd argument
 python3 aes.py -K $key
@@ -16,74 +16,76 @@ wboxkit.trace -T $nr_traces circuits/aes2-clear.bin traces/$key
 wboxkit.trace -T $nr_traces circuits/aes2-isw2.bin traces/$key
 wboxkit.trace -T $nr_traces circuits/aes2-isw3.bin traces/$key
 wboxkit.trace -T $nr_traces circuits/aes2-isw4.bin traces/$key
+wboxkit.trace -T $nr_traces circuits/aes2-isw5.bin traces/$key
 wboxkit.trace -T $nr_traces circuits/aes2-minq.bin traces/$key
 wboxkit.trace -T $nr_traces circuits/aes2-quadlin2.bin traces/$key
-wboxkit.trace -T $nr_traces circuits/aes2-quadlin3.bin traces/$key
-wboxkit.trace -T $nr_traces circuits/aes2-quadlin4.bin traces/$key
+#wboxkit.trace -T $nr_traces circuits/aes2-quadlin3.bin traces/$key
+#wboxkit.trace -T $nr_traces circuits/aes2-quadlin4.bin traces/$key
+#wboxkit.trace -T $nr_traces circuits/aes2-quadlin5.bin traces/$key
 
-# 4. run exact attack
-printf '\nExact attack on aes with 2 rounds.'
-
-printf "\nClear:"
-r="$(sage AES_ExactMatch.py traces/$key/aes2-clear/ -T 256)"
-if [[ $r == *$key* ]]; then
-    printf "key match!"
-    echo "$r"
-else
-    printf "key does not match!"
-    echo "$r"
-fi
-
-printf '\nISW-2:'
-r="$(sage AES_ExactMatch.py traces/$key/aes2-isw2/ -T 256)"
-if [[ $r == *$key* ]]; then
-    printf "key match!"
-    echo "$r"
-else
-    printf "key does not match!"
-    echo "$r"
-fi
-
-printf '\nMinq:'
-r="$(sage AES_ExactMatch.py traces/$key/aes2-minq/ -T 256)"
-if [[ $r == *$key* ]]; then
-    printf "key match!"
-    echo "$r"
-else
-    printf "key does not match!"
-    echo "$r"
-fi
-
-printf '\nLDA on aes with 2 rounds and no constant addition.'
-printf '\nClear:'
-r="$(sage AES_LDA.py traces/$key/aes2-clear/ -T 128 -W 100 -S 100)"
-if [[ $r == *$key* ]]; then
-    printf "key match!"
-    echo "$r"
-else
-    printf "key does not match!"
-    echo "$r"
-fi
-
-printf '\nISW-2:'
-r="$(sage AES_LDA.py traces/$key/aes2-isw2/ -T 200 -W 192 -S 192)"
-if [[ $r == *$key* ]]; then
-    printf "key match!"
-    echo "$r"
-else
-    printf "key does not match!"
-    echo "$r"
-fi
-
-printf '\nISW-3:'
-r="$(sage AES_LDA.py traces/$key/aes2-isw3/ -T 512 -W 500 -S 250)"
-if [[ $r == *$key* ]]; then
-    printf "key match!"
-    echo "$r"
-else
-    printf "key does not match!"
-    echo "$r"
-fi
+## 4. run exact attack
+#printf '\nExact attack on aes with 2 rounds.'
+#
+#printf "\nClear:"
+#r="$(sage AES_ExactMatch.py traces/$key/aes2-clear/ -T 256)"
+#if [[ $r == *$key* ]]; then
+#    printf "key match!"
+#    echo "$r"
+#else
+#    printf "key does not match!"
+#    echo "$r"
+#fi
+#
+#printf '\nISW-2:'
+#r="$(sage AES_ExactMatch.py traces/$key/aes2-isw2/ -T 256)"
+#if [[ $r == *$key* ]]; then
+#    printf "key match!"
+#    echo "$r"
+#else
+#    printf "key does not match!"
+#    echo "$r"
+#fi
+#
+#printf '\nMinq:'
+#r="$(sage AES_ExactMatch.py traces/$key/aes2-minq/ -T 256)"
+#if [[ $r == *$key* ]]; then
+#    printf "key match!"
+#    echo "$r"
+#else
+#    printf "key does not match!"
+#    echo "$r"
+#fi
+#
+#printf '\nLDA on aes with 2 rounds and no constant addition.'
+#printf '\nClear:'
+#r="$(sage AES_LDA.py traces/$key/aes2-clear/ -T 128 -W 100 -S 100)"
+#if [[ $r == *$key* ]]; then
+#    printf "key match!"
+#    echo "$r"
+#else
+#    printf "key does not match!"
+#    echo "$r"
+#fi
+#
+#printf '\nISW-2:'
+#r="$(sage AES_LDA.py traces/$key/aes2-isw2/ -T 200 -W 192 -S 192)"
+#if [[ $r == *$key* ]]; then
+#    printf "key match!"
+#    echo "$r"
+#else
+#    printf "key does not match!"
+#    echo "$r"
+#fi
+#
+#printf '\nISW-3:'
+#r="$(sage AES_LDA.py traces/$key/aes2-isw3/ -T 512 -W 500 -S 250)"
+#if [[ $r == *$key* ]]; then
+#    printf "key match!"
+#    echo "$r"
+#else
+#    printf "key does not match!"
+#    echo "$r"
+#fi
 
 #printf '\nMinq:'
 #r="$(sage ASCON_LDA.py traces/$key/aes2-minq/ -T 512 -W 500 -S 250)"
--- ascon_script.sh
+++ ascon_script.sh
@@ -1,53 +1,88 @@
 #!/bin/bash
 
-nr_traces=512
+nr_traces=2098
 key=abcdefghijklmnopqrstuvwxyz1234567890ABCD
+#key='0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW'
 
-## 1. run ascon128.py with key as cmd argument
-#python3 ascon128.py -K $key
-#
-## 2. mv bin to circuits folder
-#mv ./bin/ascon128_* ./circuits/
-#mkdir -p ./traces/$key
-#
-## 3. traces
-#
-##wboxkit.trace -T $nr_traces circuits/ascon128_1_no_lin_diff-clear.bin traces/
-##wboxkit.trace -T $nr_traces circuits/ascon128_1_no_lin_diff-isw_2.bin traces/
-##wboxkit.trace -T $nr_traces circuits/ascon128_1_no_lin_diff-isw_3.bin traces/
-##wboxkit.trace -T $nr_traces circuits/ascon128_1_no_lin_diff-isw_4.bin traces/
-##wboxkit.trace -T $nr_traces circuits/ascon128_1_no_lin_diff-minq.bin traces/
-##wboxkit.trace -T $nr_traces circuits/ascon128_1_no_lin_diff-ql2.bin traces/
-##wboxkit.trace -T $nr_traces circuits/ascon128_1_no_lin_diff-ql3.bin traces/
-##wboxkit.trace -T $nr_traces circuits/ascon128_1_no_lin_diff-ql4.bin traces/
-#
-##wboxkit.trace -T $nr_traces circuits/ascon128_2R_full-clear.bin traces/$key
-##wboxkit.trace -T $nr_traces circuits/ascon128_2R_full-isw_2.bin traces/$key
-##wboxkit.trace -T $nr_traces circuits/ascon128_2R_full-isw_3.bin traces/$key
-##wboxkit.trace -T $nr_traces circuits/ascon128_2R_full-isw_4.bin traces/$key
-##wboxkit.trace -T $nr_traces circuits/ascon128_2R_full-minq.bin traces/$key
-##wboxkit.trace -T $nr_traces circuits/ascon128_2R_full-ql2.bin traces/$key
-##wboxkit.trace -T $nr_traces circuits/ascon128_2R_full-ql3.bin traces/$key
-##wboxkit.trace -T $nr_traces circuits/ascon128_2R_full-ql4.bin traces/$key
-#
-#wboxkit.trace -T $nr_traces circuits/ascon128_2R_simplified-clear.bin traces/$key
-#wboxkit.trace -T $nr_traces circuits/ascon128_2R_simplified-isw_2.bin traces/$key
-#wboxkit.trace -T $nr_traces circuits/ascon128_2R_simplified-isw_3.bin traces/$key
-#wboxkit.trace -T $nr_traces circuits/ascon128_2R_simplified-isw_4.bin traces/$key
-#wboxkit.trace -T $nr_traces circuits/ascon128_2R_simplified-minq.bin traces/$key
-#wboxkit.trace -T $nr_traces circuits/ascon128_2R_simplified-ql2.bin traces/$key
+
+# 1. run ascon128.py with key as cmd argument
+python3 ascon128.py -K $key
+
+# 2. mv bin to circuits folder
+mv ./bin/ascon128_* ./circuits/
+mkdir -p ./traces/$key
+
+# 3. traces
+
+#wboxkit.trace -T $nr_traces circuits/ascon128_1_no_lin_diff-clear.bin traces/
+#wboxkit.trace -T $nr_traces circuits/ascon128_1_no_lin_diff-isw_2.bin traces/
+#wboxkit.trace -T $nr_traces circuits/ascon128_1_no_lin_diff-isw_3.bin traces/
+#wboxkit.trace -T $nr_traces circuits/ascon128_1_no_lin_diff-isw_4.bin traces/
+#wboxkit.trace -T $nr_traces circuits/ascon128_1_no_lin_diff-minq.bin traces/
+#wboxkit.trace -T $nr_traces circuits/ascon128_1_no_lin_diff-ql2.bin traces/
+#wboxkit.trace -T $nr_traces circuits/ascon128_1_no_lin_diff-ql3.bin traces/
+#wboxkit.trace -T $nr_traces circuits/ascon128_1_no_lin_diff-ql4.bin traces/
+
+#wboxkit.trace -T $nr_traces circuits/ascon128_2R_full-clear.bin traces/$key
+#wboxkit.trace -T $nr_traces circuits/ascon128_2R_full-isw_2.bin traces/$key
+#wboxkit.trace -T $nr_traces circuits/ascon128_2R_full-isw_3.bin traces/$key
+#wboxkit.trace -T $nr_traces circuits/ascon128_2R_full-isw_4.bin traces/$key
+#wboxkit.trace -T $nr_traces circuits/ascon128_2R_full-minq.bin traces/$key
+#wboxkit.trace -T $nr_traces circuits/ascon128_2R_full-ql2.bin traces/$key
+#wboxkit.trace -T $nr_traces circuits/ascon128_2R_full-ql3.bin traces/$key
+#wboxkit.trace -T $nr_traces circuits/ascon128_2R_full-ql4.bin traces/$key
+
+wboxkit.trace -T $nr_traces circuits/ascon128_2R_simplified-clear.bin traces/$key
+wboxkit.trace -T $nr_traces circuits/ascon128_2R_simplified-isw_2.bin traces/$key
+wboxkit.trace -T $nr_traces circuits/ascon128_2R_simplified-isw_3.bin traces/$key
+wboxkit.trace -T $nr_traces circuits/ascon128_2R_simplified-isw_4.bin traces/$key
+wboxkit.trace -T $nr_traces circuits/ascon128_2R_simplified-isw_5.bin traces/$key
+wboxkit.trace -T $nr_traces circuits/ascon128_2R_simplified-minq.bin traces/$key
+wboxkit.trace -T $nr_traces circuits/ascon128_2R_simplified-ql2.bin traces/$key
 #wboxkit.trace -T $nr_traces circuits/ascon128_2R_simplified-ql3.bin traces/$key
 #wboxkit.trace -T $nr_traces circuits/ascon128_2R_simplified-ql4.bin traces/$key
+#wboxkit.trace -T $nr_traces circuits/ascon128_2R_simplified-ql5.bin traces/$key
 
-# 4. run exact attack
-printf '\nExact attack on ascon with 2 rounds and no constant addition.'
-
-#sage ASCON_ExactMatch.py traces/ascon128_1_no_lin_diff-clear/ -T 256
-#sage ASCON_ExactMatch.py traces/ascon128_1_no_lin_diff-isw_2/ -T 256
-#sage ASCON_ExactMatch.py traces/ascon128_1_no_lin_diff-minq/ -T 256
-
-#printf "\nClear:"
-#r="$(sage ASCON_ExactMatch.py traces/$key/ascon128_2R_simplified-clear/ -T 256)"
+## 4. run exact attack
+#printf '\nExact attack on ascon with 2 rounds and no constant addition.'
+#
+##sage ASCON_ExactMatch.py traces/ascon128_1_no_lin_diff-clear/ -T 256
+##sage ASCON_ExactMatch.py traces/ascon128_1_no_lin_diff-isw_2/ -T 256
+##sage ASCON_ExactMatch.py traces/ascon128_1_no_lin_diff-minq/ -T 256
+#
+##printf "\nClear:"
+##r="$(sage ASCON_ExactMatch.py traces/$key/ascon128_2R_simplified-clear/ -T 256)"
+##if [[ $r == *$key* ]]; then
+##    printf "key match!"
+##    echo "$r"
+##else
+##    printf "key does not match!"
+##    echo "$r"
+##fi
+##
+##printf '\nISW-2:'
+##r="$(sage ASCON_ExactMatch.py traces/$key/ascon128_2R_simplified-isw_2/ -T 256)"
+##if [[ $r == *$key* ]]; then
+##    printf "key match!"
+##    echo "$r"
+##else
+##    printf "key does not match!"
+##    echo "$r"
+##fi
+##
+##printf '\nMinq:'
+##r="$(sage ASCON_ExactMatch.py traces/$key/ascon128_2R_simplified-minq/ -T 256)"
+##if [[ $r == *$key* ]]; then
+##    printf "key match!"
+##    echo "$r"
+##else
+##    printf "key does not match!"
+##    echo "$r"
+##fi
+#
+#printf '\nLDA on ascon with 2 rounds and no constant addition.'
+#printf '\nClear:'
+#r="$(sage ASCON_LDA.py traces/$key/ascon128_2R_simplified-clear/ -T 128 -W 100 -S 100)"
 #if [[ $r == *$key* ]]; then
 #    printf "key match!"
 #    echo "$r"
@@ -57,7 +92,7 @@ printf '\nExact attack on ascon with 2 rounds and no constant addition.'
 #fi
 #
 #printf '\nISW-2:'
-#r="$(sage ASCON_ExactMatch.py traces/$key/ascon128_2R_simplified-isw_2/ -T 256)"
+#r="$(sage ASCON_LDA.py traces/$key/ascon128_2R_simplified-isw_2/ -T 200 -W 192 -S 192)"
 #if [[ $r == *$key* ]]; then
 #    printf "key match!"
 #    echo "$r"
@@ -66,8 +101,8 @@ printf '\nExact attack on ascon with 2 rounds and no constant addition.'
 #    echo "$r"
 #fi
 #
-#printf '\nMinq:'
-#r="$(sage ASCON_ExactMatch.py traces/$key/ascon128_2R_simplified-minq/ -T 256)"
+#printf '\nISW-3:'
+#r="$(sage ASCON_LDA.py traces/$key/ascon128_2R_simplified-isw_3/ -T 512 -W 500 -S 250)"
 #if [[ $r == *$key* ]]; then
 #    printf "key match!"
 #    echo "$r"
@@ -75,37 +110,6 @@ printf '\nExact attack on ascon with 2 rounds and no constant addition.'
 #    printf "key does not match!"
 #    echo "$r"
 #fi
-
-printf '\nLDA on ascon with 2 rounds and no constant addition.'
-printf '\nClear:'
-r="$(sage ASCON_LDA.py traces/$key/ascon128_2R_simplified-clear/ -T 128 -W 100 -S 100)"
-if [[ $r == *$key* ]]; then
-    printf "key match!"
-    echo "$r"
-else
-    printf "key does not match!"
-    echo "$r"
-fi
-
-printf '\nISW-2:'
-r="$(sage ASCON_LDA.py traces/$key/ascon128_2R_simplified-isw_2/ -T 200 -W 192 -S 192)"
-if [[ $r == *$key* ]]; then
-    printf "key match!"
-    echo "$r"
-else
-    printf "key does not match!"
-    echo "$r"
-fi
-
-printf '\nISW-3:'
-r="$(sage ASCON_LDA.py traces/$key/ascon128_2R_simplified-isw_3/ -T 512 -W 500 -S 250)"
-if [[ $r == *$key* ]]; then
-    printf "key match!"
-    echo "$r"
-else
-    printf "key does not match!"
-    echo "$r"
-fi
 
 #printf '\nMinq:'
 #r="$(sage ASCON_LDA.py traces/$key/ascon128_2R_simplified-minq/ -T 512 -W 500 -S 250)"
