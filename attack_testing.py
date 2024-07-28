fddbff496b525823bc4e9c09cb10a01c207d53ab
Peral175 <perrardalex@yahoo.com>
2024-07-28 21:11:37 +0200

    work in progress

--- attack_testing.py
+++ attack_testing.py
@@ -9,47 +9,71 @@ import AES_LDA
 import ASCON_ExactMatch
 import ASCON_LDA
 
+aes_clear1 = pathlib.PosixPath("traces/abcdefghABCDEFGH/aes2-clear/")
+aes_isw2_1 = pathlib.PosixPath("traces/abcdefghABCDEFGH/aes2-isw2/")
+aes_isw3_1 = pathlib.PosixPath("traces/abcdefghABCDEFGH/aes2-isw3/")
+aes_isw4_1 = pathlib.PosixPath("traces/abcdefghABCDEFGH/aes2-isw4/")
+aes_minq_1 = pathlib.PosixPath("traces/abcdefghABCDEFGH/aes2-minq/")
+aes_ql2_1 = pathlib.PosixPath("traces/abcdefghABCDEFGH/aes2-ql2/")
+aes_ql3_1 = pathlib.PosixPath("traces/abcdefghABCDEFGH/aes2-ql3/")
+aes_ql4_1 = pathlib.PosixPath("traces/abcdefghABCDEFGH/aes2-ql4/")
+aes_cl2_1 = pathlib.PosixPath("traces/abcdefghABCDEFGH/aes2-cl2/")
+aes_cl3_1 = pathlib.PosixPath("traces/abcdefghABCDEFGH/aes2-cl3/")
+aes_cl4_1 = pathlib.PosixPath("traces/abcdefghABCDEFGH/aes2-cl4/")
+
+aes_clear2 = pathlib.PosixPath("traces/0123456789:;<=>?/aes2-clear/")
+aes_isw2_2 = pathlib.PosixPath("traces/0123456789:;<=>?/aes2-isw2/")
+aes_isw3_2 = pathlib.PosixPath("traces/0123456789:;<=>?/aes2-isw3/")
+aes_isw4_2 = pathlib.PosixPath("traces/0123456789:;<=>?/aes2-isw4/")
+aes_minq_2 = pathlib.PosixPath("traces/0123456789:;<=>?/aes2-minq/")
+aes_ql2_2 = pathlib.PosixPath("traces/0123456789:;<=>?/aes2-ql2/")
+aes_ql3_2 = pathlib.PosixPath("traces/0123456789:;<=>?/aes2-ql3/")
+aes_ql4_2 = pathlib.PosixPath("traces/0123456789:;<=>?/aes2-ql4/")
+aes_cl2_2 = pathlib.PosixPath("traces/0123456789:;<=>?/aes2-cl2/")
+aes_cl3_2 = pathlib.PosixPath("traces/0123456789:;<=>?/aes2-cl3/")
+aes_cl4_2 = pathlib.PosixPath("traces/0123456789:;<=>?/aes2-cl4/")
+
+ascon_clear1 = pathlib.PosixPath("traces/abcdefghijklmnopqrstuvwxyz1234567890ABCD/asconP_2R_NCA-clear/")
+ascon_isw2_1 = pathlib.PosixPath("traces/abcdefghijklmnopqrstuvwxyz1234567890ABCD/asconP_2R_NCA-isw2/")
+ascon_isw3_1 = pathlib.PosixPath("traces/abcdefghijklmnopqrstuvwxyz1234567890ABCD/asconP_2R_NCA-isw3/")
+ascon_isw4_1 = pathlib.PosixPath("traces/abcdefghijklmnopqrstuvwxyz1234567890ABCD/asconP_2R_NCA-isw4/")
+ascon_minq_1 = pathlib.PosixPath("traces/abcdefghijklmnopqrstuvwxyz1234567890ABCD/asconP_2R_NCA-minq/")
+ascon_ql2_1 = pathlib.PosixPath("traces/abcdefghijklmnopqrstuvwxyz1234567890ABCD/asconP_2R_NCA-ql2/")
+ascon_ql3_1 = pathlib.PosixPath("traces/abcdefghijklmnopqrstuvwxyz1234567890ABCD/asconP_2R_NCA-ql3/")
+ascon_ql4_1 = pathlib.PosixPath("traces/abcdefghijklmnopqrstuvwxyz1234567890ABCD/asconP_2R_NCA-ql4/")
+ascon_cl2_1 = pathlib.PosixPath("traces/abcdefghijklmnopqrstuvwxyz1234567890ABCD/asconP_2R_NCA-cl2/")
+ascon_cl3_1 = pathlib.PosixPath("traces/abcdefghijklmnopqrstuvwxyz1234567890ABCD/asconP_2R_NCA-cl3/")
+ascon_cl4_1 = pathlib.PosixPath("traces/abcdefghijklmnopqrstuvwxyz1234567890ABCD/asconP_2R_NCA-cl4/")
+
+ascon_clear2 = pathlib.PosixPath("traces/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW/asconP_2R_NCA-clear/")
+ascon_isw2_2 = pathlib.PosixPath("traces/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW/asconP_2R_NCA-isw2/")
+ascon_isw3_2 = pathlib.PosixPath("traces/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW/asconP_2R_NCA-isw3/")
+ascon_isw4_2 = pathlib.PosixPath("traces/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW/asconP_2R_NCA-isw4/")
+ascon_minq_2 = pathlib.PosixPath("traces/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW/asconP_2R_NCA-minq/")
+ascon_ql2_2 = pathlib.PosixPath("traces/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW/asconP_2R_NCA-ql2/")
+ascon_ql3_2 = pathlib.PosixPath("traces/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW/asconP_2R_NCA-ql3/")
+ascon_ql4_2 = pathlib.PosixPath("traces/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW/asconP_2R_NCA-ql4/")
+ascon_cl2_2 = pathlib.PosixPath("traces/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW/asconP_2R_NCA-cl2/")
+ascon_cl3_2 = pathlib.PosixPath("traces/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW/asconP_2R_NCA-cl3/")
+ascon_cl4_2 = pathlib.PosixPath("traces/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW/asconP_2R_NCA-cl4/")
+
+tr_aes_1 = [aes_clear1, aes_isw2_1, aes_isw3_1, aes_isw4_1,
+            aes_minq_1, aes_ql2_1, aes_ql3_1, aes_ql4_1,
+            aes_cl2_1, aes_cl3_1, aes_cl4_1]
+tr_aes_2 = [aes_clear2, aes_isw2_2, aes_isw3_2, aes_isw4_2,
+            aes_minq_2, aes_ql2_2, aes_ql3_2, aes_ql4_2,
+            aes_cl2_2, aes_cl3_2, aes_cl4_2]
+tr_ascon_1 = [ascon_clear1, ascon_isw2_1, ascon_isw3_1, ascon_isw4_1,
+              ascon_minq_1, ascon_ql2_1, ascon_ql3_1, ascon_ql4_1,
+              ascon_cl2_1, ascon_cl3_1, ascon_cl4_1]
+tr_ascon_2 = [ascon_clear2, ascon_isw2_2, ascon_isw3_2, ascon_isw4_2,
+              ascon_minq_2, ascon_ql2_2, ascon_ql3_2, ascon_ql4_2,
+              ascon_cl2_2, ascon_cl3_2, ascon_cl4_2]
 
 class TestStringMethods(unittest.TestCase):
-    aes_clear1 = pathlib.PosixPath("traces/abcdefghABCDEFGH/aes2-clear/")
-    aes_isw2_1 = pathlib.PosixPath("traces/abcdefghABCDEFGH/aes2-isw2/")
-    aes_isw3_1 = pathlib.PosixPath("traces/abcdefghABCDEFGH/aes2-isw3/")
-    aes_isw4_1 = pathlib.PosixPath("traces/abcdefghABCDEFGH/aes2-isw4/")
-    aes_isw5_1 = pathlib.PosixPath("traces/abcdefghABCDEFGH/aes2-isw5/")
-    aes_clear2 = pathlib.PosixPath("traces/0123456789:;<=>?/aes2-clear/")
-    aes_isw2_2 = pathlib.PosixPath("traces/0123456789:;<=>?/aes2-isw2/")
-    aes_isw3_2 = pathlib.PosixPath("traces/0123456789:;<=>?/aes2-isw3/")
-    aes_isw4_2 = pathlib.PosixPath("traces/0123456789:;<=>?/aes2-isw4/")
-    aes_isw5_2 = pathlib.PosixPath("traces/0123456789:;<=>?/aes2-isw5/")
-    # tr_aes = [aes_clear1, aes_isw2_1, aes_isw3_1, aes_isw4_1, aes_isw5_1,
-    #           aes_clear2, aes_isw2_2, aes_isw3_2, aes_isw4_2, aes_isw5_2]
-    tr_aes = [aes_clear1, aes_isw2_1, aes_clear2, aes_isw2_2]
-    ascon_clear1 = pathlib.PosixPath(
-        "traces/abcdefghijklmnopqrstuvwxyz1234567890ABCD/ascon128_2R_simplified-clear/")
-    ascon_isw2_1 = pathlib.PosixPath(
-        "traces/abcdefghijklmnopqrstuvwxyz1234567890ABCD/ascon128_2R_simplified-isw_2/")
-    ascon_isw3_1 = pathlib.PosixPath(
-        "traces/abcdefghijklmnopqrstuvwxyz1234567890ABCD/ascon128_2R_simplified-isw_3/")
-    ascon_isw4_1 = pathlib.PosixPath(
-        "traces/abcdefghijklmnopqrstuvwxyz1234567890ABCD/ascon128_2R_simplified-isw_4/")
-    ascon_isw5_1 = pathlib.PosixPath(
-        "traces/abcdefghijklmnopqrstuvwxyz1234567890ABCD/ascon128_2R_simplified-isw_5/")
-    ascon_clear2 = pathlib.PosixPath(
-        "traces/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW/ascon128_2R_simplified-clear/")
-    ascon_isw2_2 = pathlib.PosixPath(
-        "traces/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW/ascon128_2R_simplified-isw_2/")
-    ascon_isw3_2 = pathlib.PosixPath(
-        "traces/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW/ascon128_2R_simplified-isw_3/")
-    ascon_isw4_2 = pathlib.PosixPath(
-        "traces/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW/ascon128_2R_simplified-isw_4/")
-    ascon_isw5_2 = pathlib.PosixPath(
-        "traces/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW/ascon128_2R_simplified-isw_5/")
-    # tr_ascon = [ascon_clear1, ascon_isw2_1, ascon_isw3_1, ascon_isw4_1, ascon_isw5_1,
-    #             ascon_clear2, ascon_isw2_2, ascon_isw3_2, ascon_isw4_2, ascon_isw5_2]
-    tr_ascon = [ascon_clear1, ascon_isw2_1, ascon_clear2, ascon_isw2_2]
 
     t = 306
-    nr_of_runs = 2
+    nr_of_runs = 20
     nr_increments = 2
 
     def test_aes_exact(self):
@@ -111,7 +135,7 @@ class TestStringMethods(unittest.TestCase):
             target = target.encode('utf-8').hex()  # bytes.fromhex("61626364656667684142434445464748")
             for inc in range(self.nr_increments):
                 times = []
-                w_size = (inc+2)*64
+                w_size = (inc+1)*64
                 for j in range(self.nr_of_runs):         # average of 10 runs
                     t_start = time.time()
                     s_size = w_size // 2
